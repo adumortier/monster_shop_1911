3ca6da6d1aa8d938bc512095243bd6c730c512f3
Alexis Dumortier <dumortier.alexis@gmail.com>
2020-02-29 16:21:22 -0700

    Test the method that checks if a new discount is in conflict with other discounts

--- spec/models/discount_spec.rb
+++ spec/models/discount_spec.rb
@@ -12,4 +12,17 @@ RSpec.describe Discount, type: :model do
     it { should belong_to :merchant }
   end 
 
+  describe 'methods' do 
+    it '#valid_discount?' do 
+      @merchant1 = create(:random_merchant)
+      @discount1 = @merchant1.discounts.create!(name: 'winter special', number_items: 10, percent: 15)
+      @discount2 = @merchant1.discounts.create!(name: 'big sale', number_items: 5, percent: 10)
+      @discount3 = @merchant1.discounts.create!(name: 'Huge sale', number_items: 20, percent: 12)
+      expect(@discount3.valid_discount?).to eq(false)
+      @discount4 = @merchant1.discounts.create!(name: 'Huge sale', number_items: 2, percent: 12)
+      expect(@discount4.valid_discount?).to eq(false)
+    end
+  end
+
+
 end
